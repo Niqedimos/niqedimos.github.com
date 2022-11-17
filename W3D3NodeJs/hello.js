5fbc701778e240f060b9216cad6bde6d8fb193ab
Niqedimos <117125310+Niqedimos@users.noreply.github.com>
2022-11-17 08:56:15 -0600

    Add files via upload

---
+++ W3D3NodeJs/hello.js
@@ -0,0 +1,19 @@
+const { SocketAddress } = require('net');
+
+const readline=require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+readline.question("what is your name?", name=>{
+    console.log("welcome" ,name);
+    readline.question("what is your age?", age=>{
+        if(age<16){
+            console.log("You are not allowed to drive in Iowa");
+        }
+        else{
+            console.log("You are allowed to get a drivers license in Iowa");
+        }
+        readline.close();
+    })
+   
+});
